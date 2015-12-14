1cb1bee1b2077cd60a05014ef1797e82a77f1347
Yen-Ting Liu <windows.linux.mac@gmail.com>
2015-12-14 16:30:11 +0800

    Add read voltage function

--- Source/Driver/mcp4725.c
+++ Source/Driver/mcp4725.c
@@ -1,1 +1,50 @@
+/* System header */
+#include <stdint.h>
+#include <stdbool.h>
+
+/* Tiva Ware header */
+
+/* Tiva Ware driver library */
+
+/* Custom header */
+#include "i2c.h"
+
 #include "mcp4725.h"
+
+uint8_t rx_buf[5];
+#define current_dac_volt	(rx_buf[1] << 4) | (rx_buf[2] >> 4)
+#define current_eeprom_volt	((rx_buf[3] & 0x0F) << 8) | rx_buf[4] 
+
+void mcp4725_init(mcp4725 *dev, uint8_t dev_addr, i2c_speed speed) {
+	// Force degrade invalid speed mode.
+	if(speed == I2C_FAST_MODE_PLUS)
+		speed = I2C_FAST_MODE;
+	
+	// Store the value in the object.
+	dev->addr = (DEVICE_CODE << 4) | (dev_addr << 1);
+	dev->speed = speed;
+	
+	i2c_init(dev->speed);
+}
+
+void mcp4725_set_voltage(mcp4725 *dev, uint16_t volt, bool write_eeprom) {
+	// Convert the voltage to bit patterns.
+	uint8_t upper_bits = volt >> 4;
+	uint8_t lower_bits = (volt & 0x0F) << 4;
+	
+	if(write_eeprom) {
+		i2c_send(dev->addr, 3, MCP4725_WRITE_DAC_AND_EEPROM,
+							   upper_bits,
+							   lower_bits);
+	} else {
+		i2c_send(dev->addr, 3, MCP4725_WRITE_DAC,
+							   upper_bits,
+							   lower_bits);
+	}
+}
+
+void mcp4725_read_voltage(mcp4725 *dev, uint16_t *dac, uint16_t *eeprom) {
+	i2c_batch_receive(dev->addr | 0x01, 0x00, 5, rx_buf);
+	*dac = current_dac_volt;
+	*eeprom = current_eeprom_volt;
+}
--- Source/Driver/mcp4725.h
+++ Source/Driver/mcp4725.h
@@ -1,4 +1,38 @@
 #ifndef __MCP4725_H__
 #define __MCP4725_H__
 
+/* System header */
+#include <stdint.h>
+#include <stdbool.h>
+
+/* Custom header */
+#include "i2c.h"
+
+#define DEVICE_CODE						0x0C
+
+#define MCP4725_WRITE_DAC				0x40
+#define MCP4725_WRITE_DAC_AND_EEPROM	0x60
+#define MCP4725_WRITE_FAST_MODE			0x00
+
+/*
+Fast mode (repeat sequence):
+	< 0  0  PD1 PD0 D11 D10 D9  D8 >
+	< D7 D6 D5  D4  D3  D2  D1  D0 >
+
+Standard (one-shot):
+	<0   1   C0 X  X  PD1 PD0 X  >
+	<D11 D10 D9 D8 D7 D6  D5  D4 >
+	<D3  D2  D1 D0 X  X   X   X  >
+	C0: 1/0 = DAC&EEPROM/DAC
+*/
+
+typedef struct {
+	uint8_t addr;
+	i2c_speed speed;
+} mcp4725;
+
+void mcp4725_init(mcp4725 *dev, uint8_t dev_addr, i2c_speed speed);
+void mcp4725_set_voltage(mcp4725 *dev, uint16_t volt, bool write_eeprom);
+void mcp4725_read_voltage(mcp4725 *dev, uint16_t *dac, uint16_t *eeprom);
+
 #endif
