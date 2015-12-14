6c0eb4ae08ab1f01633b9819ddc018cb0ad33c36
Yen-Ting Liu <windows.linux.mac@gmail.com>
2015-12-14 16:31:03 +0800

    Update main function to use MCP4725 library

--- Source/main.c
+++ Source/main.c
@@ -1,22 +1,29 @@
-#include <stdbool.h>
+/* System header */
+#include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
 
+/* Tiva Ware header */
 #include "inc/tm4c123gh6pm.h"
 
+/* Tiva Ware driver library */
 #include "driverlib/sysctl.h"
 
+/* Custom header */
 #include "driver/i2c.h"
+#include "driver/mcp4725.h"
 
 int main(void) {
 	// Set the clock to run directly from the external oscillator.
 	SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_PLL | SYSCTL_OSC_INT | SYSCTL_XTAL_16MHZ);
 	
-	i2c_init();
+	mcp4725 *red = NULL; //, *green = NULL, *blue = NULL;
+	mcp4725_init(red, 0x00, I2C_STANDARD);
 	
 	while(1) {
-		i2c_send(0x60, 3, 0x40, 0x00, 0x00);
+		mcp4725_set_voltage(red, 0, false);
 		SysCtlDelay(16);
-		i2c_send(0x60, 3, 0x40, 0xFF, 0xF0);
+		mcp4725_set_voltage(red, 2048, false);
 		SysCtlDelay(16);
 	}
 }
